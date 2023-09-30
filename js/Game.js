141f9347dd5e26492cdb61fa671daf67a33ea55c
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-09-30 14:23:46 -0700

    added more logic to objects

--- js/Game.js
+++ js/Game.js
@@ -3,15 +3,31 @@
  * Game.js */
 
 class Game{
-    constructor(missed, phrases, activePhrase){
-        this.missed = missed;
-        this.phrases = phrases;
-        this.activePhrase = activePhrase;
+    constructor(missed, activePhrase){
+        this.missed = missed; //used to track the number of missed guesses by the player
+        this.phrases = ['hello world','beatiful day','how are you','did you guess','great work']; //an array of five Phrase objects to use with the game
+        this.activePhrase = activePhrase; //the Phrase object that’s currently in play
     }
-    startGame(){}
-    getRandomPhrase(){}
+    startGame(){
+        //hide start screen overlay
+        const startScreenOverlay = document.querySelector('#overlay');
+        startScreenOverlay.style.display = 'none';
+
+        //calls get getrandomPhrase() method
+        this.activePhrase = getRandomPhrase();
+
+        //adds that phrase to the board by calling the addPhraseToDisplay() method on the activePhrase property.
+        this.activePhrase.addPhraseToDisplay();
+    }
+    getRandomPhrase(){
+        const randomPhrase = this.phrases[Math.floor(Math.random()*this.phrases.length)];
+        return randomPhrase;
+    }
+
     handleInteraction(){}
     removeLife(){}
     checkForWin(){}
     gameOver(){}
 }
+
+const newGame = new Game();
--- js/Phrase.js
+++ js/Phrase.js
@@ -7,9 +7,22 @@ class Phrase{
         this.phrase = phrase.toLowerCase();
     }
     addPhraseToDisplay(){
-        const phrase = document.querySelector('#phrase');
-        
+        const phraseDiv = document.querySelector('#phrase');
+        const phraseUL = phraseDiv.querySelector('ul');
+        const letters = this.phrase.split('');
+        const htmlLetterElements = letters
+            .map(letter=>{
+                const lowerCaseRegex = /[a-z]/;
+                const spaceRegex = / /;
+                if(lowerCaseRegex.test(letter)){
+                    return `<li class="hide letter ${letter}">${letter}</li>`;
+                }else if(spaceRegex.test(letter)){
+                    return `<li class="space">${letter}</li>`;
+                }
+            });
+        htmlLetterElements.forEach(element => phraseUL.insertAdjacentHTML('beforeend', element));
     }
-    checkLetter(){}
-    showMatchedLetter(){}
+    
+    checkLetter(){} //checks to see if the letter selected by the player matches a letter in the phrase.
+    showMatchedLetter(){} //reveals the letter(s) on the board that matches the player's selection
 }
